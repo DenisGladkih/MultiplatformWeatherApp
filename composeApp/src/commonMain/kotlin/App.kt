be87550413ec84118fda5ffe81d7e8f436aaccee
DenisGladkih <denis.gladkih.55@yandex.ru>
2023-11-29 15:03:03 +0600

    test commit

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -23,7 +23,7 @@ fun App() {
         var showImage by remember { mutableStateOf(false) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Button(onClick = {
-                greetingText = "Compose: ${Greeting().greet()}"
+                greetingText = "Test: ${Greeting().greet()}"
                 showImage = !showImage
             }) {
                 Text(greetingText)
